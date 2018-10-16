191ba3673f1d3156aef7389ff6ed2963deb723da
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-10-16 17:31:15 +0530

    website - responsive

--- website/magicboxautomation/src/Header/Header.js
+++ website/magicboxautomation/src/Header/Header.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
+import logo from '../asset/img/logo.jpg';
 import './header.css';
 class Header extends Component{
 	render(){
--- website/magicboxautomation/src/Section/Section.js
+++ website/magicboxautomation/src/Section/Section.js
@@ -1,18 +1,21 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import hotel from '../asset/img/hotel.png';
+import Header from '../Header/Header';
 import './section.css';
 class Section extends Component{
 	render(){
 		return(
 			<div>
 			<section className='section-1'>
+			<Header />
 				<h1 className='title'>We help transforming smart hotels</h1>
 				<p className='desc'>Secure control at your fingertip with lifetime experience of comfort and luxury for your customers</p>
 			</section>
 			<section className='section-2'>
 				<h1 className='sub-title'>What we do</h1>
 				<p className='sub-desc'>End to end customized automation services for your hotels and home</p>
-				<img src='..//asset/img/hotel.png' className="hotel" />
+				<img src={hotel} className="hotel" />
 			</section>
 			<section className='section-3'></section>
 			</div>
